318b2d24f3e53447c5972b654e38e6e46c761069
Denis Kotelnikov <Denis_Kotelnikov@epam.com>
2018-03-15 14:41:16 +0300

    Fixed test data

--- src/main/resources/fillDBWithTestData.sql
+++ src/main/resources/fillDBWithTestData.sql
@@ -3,10 +3,10 @@ values('s1@mail.ru','asd123','Ivan','Ivanov','student'),
   ('s2@mail.ru','asd123','Zefir','Marmeladov','student'),
   ('t1@mail.ru','asd123','Tutor','Tutorialov','tutor');
 
-insert into courses(name, annotation, status)
-values('History of Uganda', 'Cource about history of Uganda', 'planned'),
-  ('Java', 'How to write code like a real patsan', 'planned');
+insert into courses(name, tutorId, annotation, status)
+values('History of Uganda', 3, 'Cource about history of Uganda', 'planned'),
+  ('Java', 3, 'How to write code like a real patsan', 'planned');
 
 insert into student_course(courseId, studentId)
-values((select id from courses where name='Java'),(select id from users where name='Ivan')),
-  ((select id from courses where name='History of Uganda'),(select id from users where name='Zefir'));
+values((select courses.courseId from courses where name='Java'),(select userId from users where name='Ivan')),
+  ((select courses.courseId from courses where name='History of Uganda'),(select userId from users where name='Zefir'));
