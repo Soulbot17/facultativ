2b874aa4bb6e348ec071d1dfc67770811412064b
Denis Kotelnikov <Denis_Kotelnikov@epam.com>
2018-03-15 14:51:24 +0300

    CAPS LOCK on

--- src/main/resources/dataBaseInit.sql
+++ src/main/resources/dataBaseInit.sql
@@ -1,29 +1,29 @@
 create table users(
-  userId int AUTO_INCREMENT,
-  email varchar(45) not null,
-  pass varchar(45) not null,
-  name varchar(45),
-  lastName varchar(45),
-  role varchar(45) not null,
+  userId INT AUTO_INCREMENT,
+  email VARCHAR(45) NOT NULL,
+  pass VARCHAR(45) NOT NULL,
+  name VARCHAR(45),
+  lastName VARCHAR(45),
+  role VARCHAR(45) NOT NULL,
   PRIMARY KEY (userId)
 );
 
 create table courses(
-  courseId int AUTO_INCREMENT,
-  name varchar(45) not null,
-  tutorId int not null,
-  annotation varchar(255),
-  status varchar(45),
+  courseId INT AUTO_INCREMENT,
+  name VARCHAR(45) NOT NULL,
+  tutorId INT NOT NULL,
+  annotation VARCHAR(255),
+  status VARCHAR(45),
   PRIMARY KEY (courseId),
   FOREIGN KEY (tutorId) REFERENCES users(userId)
 );
 
 create table student_course(
-  id int auto_increment,
-  courseId int not null,
-  studentId int not null,
-  studentMark int,
-  studentFeedback varchar(255),
+  id INT AUTO_INCREMENT,
+  courseId INT NOT NULL,
+  studentId INT NOT NULL,
+  studentMark INT,
+  studentFeedback VARCHAR(255),
   PRIMARY KEY (id),
   FOREIGN KEY (studentId) REFERENCES users(userId),
   FOREIGN KEY (courseId) REFERENCES courses(courseId)
