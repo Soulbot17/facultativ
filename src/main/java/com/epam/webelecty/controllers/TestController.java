7113e4e8f0c5b4a2e8aed645a262e7ce07138632
Egor Vyrva <Egor_Vyrva@epam.com>
2018-03-18 04:04:20 +0300

    authentification magic

--- src/main/java/com/epam/webelecty/controllers/TestController.java
+++ src/main/java/com/epam/webelecty/controllers/TestController.java
@@ -1,8 +1,26 @@
 package com.epam.webelecty.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
+
+    @RequestMapping(value = {"/", "/index"}, method = { RequestMethod.GET })
+    public ModelAndView getWelcomePage(){
+        return new ModelAndView("index");
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public ModelAndView getUserPage(){
+        return new ModelAndView("user");
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView getLoginPage(){
+        return new ModelAndView("login1");
+    }
 
 }
---
+++ src/main/webapp/WEB-INF/pages/user.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: banka
+  Date: 18.03.18
+  Time: 1:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>UserPage</title>
+</head>
+<body>
+    <h1>Users info</h1>
+</body>
+</html>
