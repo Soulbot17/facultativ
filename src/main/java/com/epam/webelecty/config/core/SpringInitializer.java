123b41112f981b715544dceb233e9e3b4e3e0356
Evgenii Krysenko <Evgenii_Krysenko@epam.com>
2018-03-19 18:00:25 +0300

    Add AppConfig.java, DBConfig.java, SpringInitializer.java

---
+++ src/main/java/com/epam/webelecty/config/core/SpringInitializer.java
@@ -0,0 +1,22 @@
+package com.epam.webelecty.config.core;
+
+import com.epam.webelecty.config.AppConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
