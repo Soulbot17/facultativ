9714691ec4dfe3adf82e6bf92a7a779eb0b76ac3
Denis Kotelnikov <Denis_Kotelnikov@epam.com>
2018-03-24 23:34:51 +0300

    Выпилил курс статус "active" за ненадобностью                     На странице тьютора при нажатии кнопки "finish" она сереет и становится некликабельна                     Добавил доп проверки на пустоту строк                     Пролил на штаны кофе

--- src/main/resources/db/dataBaseInitH2.sql
+++ src/main/resources/db/dataBaseInitH2.sql
@@ -6,7 +6,7 @@ CREATE TABLE test.users (
   pass     VARCHAR(244) NOT NULL,
   name     VARCHAR(45),
   lastName VARCHAR(45),
-  role     VARCHAR(10)  NOT NULL,
+  role     VARCHAR(45)  NOT NULL,
   PRIMARY KEY (userId)
 );
 
@@ -30,3 +30,55 @@ CREATE TABLE test.student_course (
   FOREIGN KEY (studentId) REFERENCES test.users (userId),
   FOREIGN KEY (courseId) REFERENCES test.courses (courseId)
 );
+
+INSERT INTO TEST.USERS (USERID, EMAIL, PASS, NAME, LASTNAME, ROLE)
+VALUES (1, 's1@mail.ru', '$2a$05$yJyr/jn9Fppmpy.9xd.gcOMqbzW/R4IMrKJhsqN/fvRjt4siXFnPG', 'Ivan', 'Ivanov', 'student');
+INSERT INTO TEST.USERS (USERID, EMAIL, PASS, NAME, LASTNAME, ROLE) VALUES
+  (2, 's2@mail.ru', '$2a$05$yJyr/jn9Fppmpy.9xd.gcOMqbzW/R4IMrKJhsqN/fvRjt4siXFnPG', 'Zefir', 'Marmeladov', 'student');
+INSERT INTO TEST.USERS (USERID, EMAIL, PASS, NAME, LASTNAME, ROLE) VALUES
+  (3, 't1@mail.ru', '$2a$05$yJyr/jn9Fppmpy.9xd.gcOMqbzW/R4IMrKJhsqN/fvRjt4siXFnPG', 'Tutor', 'Tutorialov', 'tutor');
+INSERT INTO TEST.USERS (USERID, EMAIL, PASS, NAME, LASTNAME, ROLE) VALUES
+  (4, 't2@mail.ru', '$2a$05$yJyr/jn9Fppmpy.9xd.gcOMqbzW/R4IMrKJhsqN/fvRjt4siXFnPG', 'Tutor2', 'Tutorialov2', 'tutor');
+INSERT INTO TEST.USERS (USERID, EMAIL, PASS, NAME, LASTNAME, ROLE) VALUES
+  (5, 's3@mail.ru', '$2a$05$yJyr/jn9Fppmpy.9xd.gcOMqbzW/R4IMrKJhsqN/fvRjt4siXFnPG', 'Denis', 'Kotelnikov', 'student');
+INSERT INTO TEST.USERS (USERID, EMAIL, PASS, NAME, LASTNAME, ROLE)
+VALUES (6, 's4@mail.ru', '$2a$05$yJyr/jn9Fppmpy.9xd.gcOMqbzW/R4IMrKJhsqN/fvRjt4siXFnPG', 'Egor', 'Vyrva', 'student');
+INSERT INTO TEST.USERS (USERID, EMAIL, PASS, NAME, LASTNAME, ROLE) VALUES
+  (7, 's5@mail.ru', '$2a$05$yJyr/jn9Fppmpy.9xd.gcOMqbzW/R4IMrKJhsqN/fvRjt4siXFnPG', 'Evgeniy', 'Krysenko', 'student');
+INSERT INTO TEST.USERS (USERID, EMAIL, PASS, NAME, LASTNAME, ROLE)
+VALUES (8, 's6@mail.ru', '$2a$05$yJyr/jn9Fppmpy.9xd.gcOMqbzW/R4IMrKJhsqN/fvRjt4siXFnPG', 'Igor', 'Balabaev', 'student');
+INSERT INTO TEST.USERS (USERID, EMAIL, PASS, NAME, LASTNAME, ROLE) VALUES
+  (9, 't3@mail.ru', '$2a$05$yJyr/jn9Fppmpy.9xd.gcOMqbzW/R4IMrKJhsqN/fvRjt4siXFnPG', 'Grigory', 'Rasputin', 'tutor');
+
+INSERT INTO TEST.COURSES (COURSEID, NAME, TUTORID, ANNOTATION, STATUS)
+VALUES (1, 'History of Uganda', 3, 'Cource about history of Uganda', 'planned');
+INSERT INTO TEST.COURSES (COURSEID, NAME, TUTORID, ANNOTATION, STATUS)
+VALUES (2, 'Java', 3, 'How to write code like a real patsan', 'planned');
+INSERT INTO TEST.COURSES (COURSEID, NAME, TUTORID, ANNOTATION, STATUS)
+VALUES (3, 'Git', 4, 'Learn Git branching', 'planned');
+INSERT INTO TEST.COURSES (COURSEID, NAME, TUTORID, ANNOTATION, STATUS)
+VALUES (4, 'Jedi', 9, 'How to fight like a jedi', 'planned');
+INSERT INTO TEST.COURSES (COURSEID, NAME, TUTORID, ANNOTATION, STATUS)
+VALUES (5, 'Street racing', 4, 'Just don''t drive into the wall', 'planned');
+INSERT INTO TEST.COURSES (COURSEID, NAME, TUTORID, ANNOTATION, STATUS)
+VALUES (6, 'Paleontology ', 3, 'Paleontology for python developers', 'planned');
+INSERT INTO TEST.COURSES (COURSEID, NAME, TUTORID, ANNOTATION, STATUS)
+VALUES (7, 'Composers of 15th century', 3, 'Influence of Franz Schubert to nowadays music', 'planned');
+INSERT INTO TEST.COURSES (COURSEID, NAME, TUTORID, ANNOTATION, STATUS)
+VALUES (8, 'Mimicry', 4, 'How to look like a tree', 'planned');
+INSERT INTO TEST.COURSES (COURSEID, NAME, TUTORID, ANNOTATION, STATUS)
+VALUES (9, 'Bada-boom', 9, 'Short history of magic', 'planned');
+INSERT INTO TEST.COURSES (COURSEID, NAME, TUTORID, ANNOTATION, STATUS)
+VALUES (10, 'Big bada-boom!', 9, 'Long history of magic', 'planned');
+
+INSERT INTO test.student_course (courseId, studentId)
+VALUES ((SELECT test.courses.courseId
+         FROM test.courses
+         WHERE name = 'Java'), (SELECT userId
+                                FROM test.users
+                                WHERE name = 'Ivan')),
+  ((SELECT test.courses.courseId
+    FROM test.courses
+    WHERE name = 'History of Uganda'), (SELECT userId
+                                        FROM test.users
+                                        WHERE name = 'Zefir'));
